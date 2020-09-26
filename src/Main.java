6ddc05c531509059ff15ccce2ea4cb5bebf0d7b6
Thai Nguyen <thainguyen.uet@gmail.com>
2020-09-26 22:12:43 +0700

    Add test comment.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+    /**
+     * Main function.
+     * @param args Command Line parameter.
+     */
     public static void main(String[] args){
+
         DictionaryCommandLine.dictionaryBasic();
         DictionaryCommandLine.dictionaryAdvanced();
     }
