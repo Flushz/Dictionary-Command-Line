b06e76ae03a4ff85e3b1929edfa3e117f5e96965
Thai Nguyen <thainguyen.uet@gmail.com>
2020-10-01 00:47:26 +0700

    Optimized code.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
-    /**
-     * Main function.
-     * @param args Command Line parameter.
-     */
+
     public static void main(String[] args){
 
         DictionaryCommandLine.dictionaryBasic();
